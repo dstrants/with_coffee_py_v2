ca114eeba3e262d6b63a2f5f01089ab2c6d21bbd
Dimitris <dstrants@gmail.com>
2021-06-08 10:29:16 +0300

    ✨  First version of hn finished

--- core/clients/hackernews.py
+++ core/clients/hackernews.py
@@ -1,17 +1,24 @@
 import requests
+from core.models.hackernews import Article
 
 
 class HackerNews:
-    def __init__(self) -> None:
+    def __init__(self, limit: int = 5) -> None:
         self.base_url = "https://hacker-news.firebaseio.com/v0"
+        self.articles = self.get_articles(limit=limit)
+    
 
+    def get_articles(self, limit: int = 5) -> list[Article]:
+        ids = self.top_stories()[:limit]
+        articles = [Article(**self.article(a_id)) for a_id in ids]
+        return articles
     
     def call(self, endpoint: str) -> dict:
-        response = requests.get(f"{self.base_url}/{endpoint}")
+        response = requests.get(f"{self.base_url}/{endpoint}.json")
         response.raise_for_status()
         return response.json()
     
-    def maxid(self) -> dict:
+    def max_id(self) -> dict:
         return self.call("maxitem")
     
     def new_stories(self) -> dict:
